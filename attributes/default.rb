bff24c6cba9ecac300bb8064f73dbc152d9515a0
paustin01 <paustin@biaprotect.com>
2013-10-07 12:15:10 -0700

    [IT-4586] Update for outdated glueoutputbuf option

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,4 +24,4 @@ default['redis']['daemonize']    = "no"
 default['redis']['timeout']      = "300"
 default['redis']['loglevel']     = "notice"
 default['redis']['password']     = nil
-node['redis']['appendfsync']     = "always"
+default['redis']['appendfsync']  = "always"
